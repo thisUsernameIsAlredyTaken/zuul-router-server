192c94d75486f7c214c2972986cf24348073b51a
Zhivaev Artem <zhivaev993@gmail.com>
2020-02-26 09:26:01 +0300

    allowed origin added

--- src/main/java/com/example/router/RouterApplication.java
+++ src/main/java/com/example/router/RouterApplication.java
@@ -1,22 +1,25 @@
 package com.example.router;
 
-import com.netflix.zuul.context.RequestContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import javax.servlet.http.Cookie;
-
 @SpringBootApplication
 @EnableZuulProxy
 public class RouterApplication {
+
+//    @Configuration
+//    public static class WebSecConfig extends WebSecurityConfigurerAdapter {
+//
+//        @Override
+//        protected void configure(HttpSecurity http) throws Exception {
+//            http.cors().and().csrf().disable();
+//        }
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(RouterApplication.class, args);
@@ -27,9 +30,15 @@ public class RouterApplication {
         final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         final CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("**");
+        config.addAllowedOrigin("http://localhost:4200");
+        config.addAllowedOrigin("http://localhost:4201");
+        config.addAllowedOrigin("http://127.0.0.1:4200");
+        config.addAllowedOrigin("http://127.0.0.1:4201");
         config.addAllowedHeader("**");
+        config.addAllowedHeader("*");
         config.addAllowedMethod("OPTIONS");
+        config.addAllowedMethod("**");
+        config.addAllowedMethod("*");
         config.addAllowedMethod("HEAD");
         config.addAllowedMethod("GET");
         config.addAllowedMethod("PUT");
@@ -39,4 +48,17 @@ public class RouterApplication {
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
+
+
+//    @Bean
+//    public CorsConfigurationSource corsConfigurationSource() {
+//        CorsConfiguration configuration = new CorsConfiguration();
+//        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+//        configuration.addAllowedHeader("*");
+//        configuration.addAllowedMethod("*");
+//        configuration.setAllowCredentials(true);
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", configuration);
+//        return source;
+//    }
 }
